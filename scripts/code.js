f45e1b43a34f212dbd2fd001d87ab862a512de41
Andres Prieto-Moreno <andres@iearobotics.com>
2023-10-25 20:24:10 -0700

    adding the controls behavior

--- scripts/code.js
+++ scripts/code.js
@@ -1,15 +1,51 @@
 
 // This function resets the values to start modeling
+// This function performs the conversions that are needed for the calculations
 function start() {
   document.getElementById("torpedo_count").value = 0; 
   document.getElementById("claw_count").value = 0; 
   document.getElementById("arm_count").value = 0; 
+
+  // Make sure that all paremeters were converted 
+  convert_to_liters("Tank_volume","Tank_volume_l");
+  convert_to_liters("System_pneumatic_volume","System_pneumatic_volume_l");
+  convert_to_liters("Actuators_air_loss","Actuators_air_loss_l");
+  convert_to_atm("Tank_max_pressure","Tank_max_pressure_atm");
+  convert_to_atm("Tank_min_pressure","Tank_min_pressure_atm");
+  convert_to_K("temperature_input","temperature_input_K");
+
+  // Enable the operations buttons
+  controls_disabled(false);
+
+  // Run the maths
   calculate();
 }
 
 
+
+
+// This functions resets the system parameters to defaults values
+function set_defaults() {
+  document.getElementById("Tank_volume").value = 36 ;
+  document.getElementById("Tank_max_pressure").value = 3000 ;
+  document.getElementById("Tank_min_pressure").value = 60 ;
+  document.getElementById("System_pneumatic_volume").value = 2 ;
+  document.getElementById("Actuators_air_loss").value = 1 ;
+
+  // convert to working units
+  convert_to_liters("Tank_volume","Tank_volume_l");
+  convert_to_liters("System_pneumatic_volume","System_pneumatic_volume_l");
+  convert_to_liters("Actuators_air_loss","Actuators_air_loss_l");
+  convert_to_atm("Tank_max_pressure","Tank_max_pressure_atm");
+  convert_to_atm("Tank_min_pressure","Tank_min_pressure_atm");
+
+  // Disable the operations controrls since system has changed
+  controls_disabled(true);
+}
+
 // This function executes submarine model
 function calculate() {
+
   tank_volume = parseInt(document.getElementById("Tank_volume").value);
   system_volume = parseInt(document.getElementById("System_pneumatic_volume").value);
   tank_max_pressure = parseInt(document.getElementById("Tank_max_pressure").value);
@@ -58,14 +94,16 @@ function depth_change() {
   document.getElementById("depth_output").value = document.getElementById("depth_input").value;  
 }
 
-// This functions resets the system parameters to defaults values
-function defaults() {
-  document.getElementById("Tank_volume").value = 36 ;
-  document.getElementById("Tank_max_pressure").value = 3000 ;
-  document.getElementById("Tank_min_pressure").value = 60 ;
-  document.getElementById("System_pneumatic_volume").value = 2 ;
-  document.getElementById("Actuators_air_loss").value = 1 ;
-}
+// Enable / Disable the control buttons
+ // Enable the operations buttons
+ // value = TRUE -> disables
+ // value = FALSE -> enablesd=
+ function controls_disabled(value) {
+  document.getElementById("btn_torpedo").disabled=value;
+  document.getElementById("btn_arm").disabled=value;
+  document.getElementById("btn_claw").disabled=value;
+ }
+
 
 function move_claw() {
   tmp = parseInt(document.getElementById("claw_count").value);
@@ -92,12 +130,16 @@ function convert_to_liters (id,id_out) {
   tmp = parseFloat(document.getElementById(id).value);
   tmp = tmp * 0.016387064 ;
   document.getElementById(id_out).value = tmp.toFixed(4); 
+  // system changed
+  controls_disabled(true);
 }
 
 function convert_to_atm (id,id_out) {
   tmp = parseFloat(document.getElementById(id).value);
   tmp = tmp * 0.0680459639 ;
   document.getElementById(id_out).value = tmp.toFixed(4); 
+  // system changed
+  controls_disabled(true);
 }
 
 function convert_to_K (id,id_out) {
